aea49fc4bcb85ee99f75bcadc366f9db9333043f
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-18 22:46:03 +0530

    if statement

---
+++ lec3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//Conditional statements
+int main(){
+    
+    string palcement;
+
+    // // if statement
+    // cout<<"Placement lagi ki nahi??"<<endl;
+
+    // cin >> palcement;
+
+    // if(palcement == "ha"){
+    //     cout << "Kya baat hai bhai!!" <<endl;
+    // }
+    // cout << "Nahi bhai abhi bhi Berozgaar hu :(" << endl;
+
+
+
+}
