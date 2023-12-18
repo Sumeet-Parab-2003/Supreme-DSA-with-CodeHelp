147beddb6abc12490f9a6ef953ccafa0e39209b9
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 00:17:56 +0530

    Taking Input

--- lec2.cpp
+++ lec2.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
-// int main(){
-//      cout << "Hello World" << endl;
-// }
+int main(){
+     // cout << "Hello World" << endl;
+
+     //Taking Input
+     int a;
+     cout << "Enter a number" << endl;
+     cin >> a;
+     cout<< " you entered " << a << " number"<< endl;
+}
    
