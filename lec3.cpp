1f44ec7428fa6c153acb3b9a49d21f90046874b3
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-18 23:28:57 +0530

    if else-if else statement

--- lec3.cpp
+++ lec3.cpp
@@ -17,17 +17,42 @@ int main(){
 
 
     // if else 
-     cout<<"Placement lagi ki nahi??"<<endl;
+    //  cout<<"Placement lagi ki nahi??"<<endl;
 
-    cin >> palcement;
+    // cin >> palcement;
 
-    if(palcement == "ha"){
-        cout << "Kya baat hai bhai!!" <<endl;
-    }
-     else{//nah(palcement == "nahi")
-        cout << "Nahi bhai abhi bhi Berozgaar hu :(" << endl;
-     }
+    // if(palcement == "ha"){
+    //     cout << "Kya baat hai bhai!!" <<endl;
+    // }
+    //  else{//nah(palcement == "nahi")
+    //     cout << "Nahi bhai abhi bhi Berozgaar hu :(" << endl;
+    //  }
             
-    
+    //if else-if if
+    int marks;
+    cout << "Enter Marks" <<endl;
+    cin >> marks;
+        
+    if(marks>=90){
+        cout << "your grade is A";
+    }
+    else if (marks>=80)
+    {
+        cout << "your grade is B";
+    }
+    else if (marks>=70)
+    {
+        cout << "your grade is C";
+    }
+    else if (marks>=60)
+    {
+        cout << "your grade is D";
+    }
+    else if (marks>=50)
+    {
+        cout << "your grade is E";
+    }
+    else
+        cout << "You are Failed";
     
 }
