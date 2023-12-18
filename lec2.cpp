2032f91a073c16168938841522b096297b1901e0
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 00:31:35 +0530

    Data types

--- lec2.cpp
+++ lec2.cpp
@@ -4,9 +4,26 @@ int main(){
      // cout << "Hello World" << endl;
 
      //Taking Input
-     int a;
-     cout << "Enter a number" << endl;
-     cin >> a;
-     cout<< " you entered " << a << " number"<< endl;
+    //  int a;
+    //  cout << "Enter a number" << endl;
+    //  cin >> a;
+    //  cout<< " you entered " << a << " number"<< endl;
+
+    //Data types
+    int a = 5;
+    cout << "value of a is: " << a << endl;
+
+    char ch = 'a';
+    cout << "value of ch is: " << ch << endl;
+
+    bool b = true;
+    cout << "value of b is: " << b << endl;
+
+    float c = 1.322;
+    cout << "value of c is: " << c << endl;
+
+    double d = 1.3434455353509;       // dounle has more range than float
+     cout << "value of d is: " << d << endl;
+
 }
    
