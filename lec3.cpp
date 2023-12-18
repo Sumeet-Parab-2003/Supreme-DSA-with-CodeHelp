2c67fd9e0b5933481e6c7a32653bf6368e63cbf6
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-18 23:39:49 +0530

    if else-if else statement example

--- lec3.cpp
+++ lec3.cpp
@@ -29,30 +29,41 @@ int main(){
     //  }
             
     //if else-if if
-    int marks;
-    cout << "Enter Marks" <<endl;
-    cin >> marks;
+//     int marks;
+//     cout << "Enter Marks" <<endl;
+//     cin >> marks;
         
-    if(marks>=90){
-        cout << "your grade is A";
-    }
-    else if (marks>=80)
-    {
-        cout << "your grade is B";
-    }
-    else if (marks>=70)
-    {
-        cout << "your grade is C";
-    }
-    else if (marks>=60)
-    {
-        cout << "your grade is D";
-    }
-    else if (marks>=50)
-    {
-        cout << "your grade is E";
-    }
-    else
-        cout << "You are Failed";
+//     if(marks>=90){
+//         cout << "your grade is A";
+//     }
+//     else if (marks>=80)
+//     {
+//         cout << "your grade is B";
+//     }
+//     else if (marks>=70)
+//     {
+//         cout << "your grade is C";
+//     }
+//     else if (marks>=60)
+//     {
+//         cout << "your grade is D";
+//     }
+//     else if (marks>=50)
+//     {
+//         cout << "your grade is E";
+//     }
+//     else
+//         cout << "You are Failed";
     
-}
+    int bro_num;
+    cout << "Enter no. of brothers"<< endl;
+    cin>> bro_num;
+    
+    if(bro_num==0){
+        cout << "Baat banjayegi";
+    }
+    else 
+     cout << "me maar khalunga";
+
+ }
+
