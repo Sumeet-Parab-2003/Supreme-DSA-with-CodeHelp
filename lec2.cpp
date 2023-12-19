fca77d729d060218b2d166fcb8ba48c41db85a0b
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 14:11:36 +0530

    Basics of For loops

--- lec2.cpp
+++ lec2.cpp
@@ -10,20 +10,29 @@ int main(){
     //  cout<< " you entered " << a << " number"<< endl;
 
     //Data types
-    int a = 5;
-    cout << "value of a is: " << a << endl;
+//     int a = 5;
+//     cout << "value of a is: " << a << endl;
 
-    char ch = 'a';
-    cout << "value of ch is: " << ch << endl;
+//     char ch = 'a';
+//     cout << "value of ch is: " << ch << endl;
 
-    bool b = true;
-    cout << "value of b is: " << b << endl;
+//     bool b = true;
+//     cout << "value of b is: " << b << endl;
 
-    float c = 1.322;
-    cout << "value of c is: " << c << endl;
+//     float c = 1.322;
+//     cout << "value of c is: " << c << endl;
 
-    double d = 1.3434455353509;       // dounle has more range than float
-     cout << "value of d is: " << d << endl;
+//     double d = 1.3434455353509;       // dounle has more range than float
+//      cout << "value of d is: " << d << endl;
 
+     //For Loops
+
+     // for(int i=0 ; i<=10 ;i++ ){
+     //      cout << "2*"<<i<<"=" <<2*i << endl;
+     // }
+
+     for(int i=1 ; i<=100; i= i*2){
+          cout << i << endl;
+     }
 }
    
