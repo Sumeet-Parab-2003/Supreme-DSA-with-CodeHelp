bf1cc8dda5836e3ddf4a007c6bd7ed935dd3a1c8
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 14:14:22 +0530

    Basics of For loops

--- lec2.cpp
+++ lec2.cpp
@@ -31,7 +31,11 @@ int main(){
      //      cout << "2*"<<i<<"=" <<2*i << endl;
      // }
 
-     for(int i=1 ; i<=100; i= i*2){
+     // for(int i=1 ; i<=100; i= i*2){
+     //      cout << i << endl;
+     // }
+
+     for(int i=1000 ; i>0 ; i= i/2 ){
           cout << i << endl;
      }
 }
