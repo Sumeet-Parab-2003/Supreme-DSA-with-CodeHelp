7dd78d95ce5a51a8c07779e031c10596ae2deb2b
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 16:45:26 +0530

    numeric Inverted half pyramid

--- lec3.cpp
+++ lec3.cpp
@@ -179,14 +179,25 @@ int main(){
     // }
 
    // Numeric Half Pyramid
+    // int n,k;
+    // cout << "enter no. of rows: " << endl;
+    // cin >>n; 
+    // for(int i=0; i<n ;i++){
+    //     for(int j=0; j<i+1 ; j++){
+    //         cout<<j+1 <<" ";
+    //     }
+    //     cout<< endl;
+    // }
+
+   // Numeric Inverted half Pyramid
     int n,k;
     cout << "enter no. of rows: " << endl;
-    cin >>n; 
-    for(int i=0; i<n ;i++){
-        for(int j=0; j<i+1 ; j++){
-            cout<<j+1 <<" ";
+    cin >>n;
+    for (int i = 0; i<n ; i++ ){
+        for (int j=0; j< n-i ; j++){
+            cout << j+1 << " ";
         }
-        cout<< endl;
+        cout << endl;
     }
 }
    
