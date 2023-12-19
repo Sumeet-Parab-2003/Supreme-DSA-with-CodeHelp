1528d771104124ef2f4554d166fbce2836c3fd4e
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-19 16:37:33 +0530

    inverted half pyramid

--- lec3.cpp
+++ lec3.cpp
@@ -171,10 +171,11 @@ int main(){
     int n,k;
     cout << "enter no. of rows: " << endl;
     cin >>n;
-    for (int i = 0; i<n , i++ ){
-        for (int j=0; j< n-1 ; j++){
-            
+    for (int i = 0; i<n ; i++ ){
+        for (int j=0; j< n-i ; j++){
+            cout << "* ";
         }
+        cout << endl;
     }
 }
    
