99a04fc31332289bcec1308d156aed6320f63b05
Sumeet Parab <sumeetparab4113@gmail.com>
2023-12-24 15:08:56 +0530

    debug1 1

---
+++ debug1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+
+//1) The below code snippet has some errors. Let’s debug it and make it compile & run successfully.
+// int main() {
+// 	int i=0;
+// 	i=i+1;
+// 	cout<<i;
+// 	/*print i where i=i+1 */
+// 	cout<<++i;
+// }
+
+// 2) Below code should print the sum of ‘i’ and ‘j’.
